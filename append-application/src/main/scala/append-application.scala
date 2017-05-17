75d5d57f2947bf30013592551903cfa7a1139d89
cristina-grosu <cristina.grosu@bigstep.com>
2017-05-17 12:31:13 +0300

    Update append-application.scala

--- append-application/src/main/scala/append-application.scala
+++ append-application/src/main/scala/append-application.scala
@@ -19,40 +19,24 @@ import sys.process._
 object appendObject{
 
   def main(args: Array[String]): Unit = {
-   if (args.length != 7) {
+   if (args.length != 5) {
       System.err.println(s"""
-        |Usage: append <microbatch_interval> <application_name> <spark_master> <kafka_brokers> <kafka_topics>
-        |  <microbatch_interval> is the amount of time in seconds between two succesive runs of the job
-        |  <application_name> is the name of the application as it will appear in YARN. It will be prefixed with "microbatch"
-        |  <spark_master> is the connection string to the application master
-        |  <kafka_brokers> is the connection string to the kafka kafka_brokers
-        |  <kafka_topic> is the name of the topic to read from
-        |  <kafka_offset> is the offset to start from: latest OR earliest
-        |  <kafka_offset> this is the destination url to save the table. Can be file, hdfs, bigstep datalake
+        datalake
                 """.stripMargin)
       System.exit(1)
     }
         //read arguments
-        val Array(micro_time,app_name, spark_master, kafka_brokers, kafka_topic, kafka_offset,url) = args
+        val Array(app_name, spark_master, url, dest_url , table_to_update) = args
 
         //intialize Spark Session
-        val spark = SparkSession.builder().master(spark_master).appName("streaming"+app_name).enableHiveSupport().getOrCreate()
+        val spark = SparkSession.builder().master(spark_master).appName(app_name).enableHiveSupport().getOrCreate()
 
         import spark.implicits._
-        val lines = dataStream.selectExpr("CAST(value AS STRING)").as[(String)]
+    
+        val data = spark.read.option("header", "true").option("inferSchema", "true").option("delimiter", "|").option("parserLib", "univocity").csv(url)
+       
         //Write to Table
-       val write_to_table = lines.writeStream.outputMode("append").format("parquet").queryName(kafka_topic).option("path",url).option("checkpointLocation","/checkpoint").start()
-        //val write_to_table = lines.writeStream.outputMode("append").format("memory").queryName(kafka_topic).start()
-
-        //Write Memory table to Physical SparkSQL tables periodically
-        //val selectall_query = "SELECT * FROM "
-        //val timer = new Timer("Rewrite Table", true)
-        //timer.schedule(new TimerTask{
-          //      override def run() {
-            //            spark.sql(selectall_query+kafka_topic).write.format("parquet").mode("overwrite").saveAsTable("managed_table_"+kafka_topic)
-              //  }
-        //}, batch_interval, batch_interval)
-        write_to_table.awaitTermination()
-        }
-
+       val write_to_table = data.write.mode("append").format("parquet").option("path",dest_url).saveAsTable(table_to_update)
+       
+       
 }
